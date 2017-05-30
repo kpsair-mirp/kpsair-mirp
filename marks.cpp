e0044af730b935a7f9fedab8092939025762a43f
pavan <kpsair-mirp@mit.edu>
2017-05-31 04:45:38 +0530

    Adding lots of files

---
+++ marks.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int Marks,cutoffA,cutoffB,cutoffC;
+     cout<<"enter marks of students:"<<endl;
+     cin>>Marks;
+     cout<<"enter coyoffs"<<endl;
+     cin >> cutoffA>>cutoffB>>cutoffC;
+     if (mark > cutoffA){
+     cout<<"grade A"<<endl;
+     else if (mark > cutoffB){
+     cout<<"grade B"<<endl;
+     else if (mark > cutoffC){
+     cout<<"grade C"<<endl;
+     
+     return 0
+  }
