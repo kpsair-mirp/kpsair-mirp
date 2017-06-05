f98fab1c27649c52b145baa2fa62d8f9271d313a
ndutta-mirp <ndutta-mirp@mit.edu>
2017-06-05 05:48:19 +0530

    name

---
+++ name.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int x[5]={100,2,3,4,5};
+for(int i=0;i<5;i++)
+cout<<x[i]<<" ";
+
+  char name[50]="my_name";
+
+ //print using loop
+
+cout <<"enter first name"<<endl;
+cin>>name;
+
+//print name using loop "Don't use cout<<name" 
+return 0;
+}
---
+++ name2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int x[5]={100,2,3,4,5};
+for(int i=0;i<5;i++)
+cout<<x[i]<<" ";
+
+  char name[50]="my_name";
+
+
+cout <<"enter first name"<<endl;
+cin>>name;
+
+return 0;
+}
